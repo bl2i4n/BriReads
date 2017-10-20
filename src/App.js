03dce928bc457aa2cec8d8a4bc3b89157d75db4b
bl2i4n <bl2i4n@yahoo.com>
2017-10-19 19:11:42 -0700

    working on main page state for books

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -10,7 +10,24 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+
+     books: []
+
+    //  showSearchPage: false
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    })
+  }
+
+  moveBook = (book) => {
+    this.setState((state) => ({
+      books: //need to find book id.
+             //watch video portion of removing contacts again
+    }))
+
   }
 
   render() {
