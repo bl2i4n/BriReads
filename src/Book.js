b8ba452a36a6bab8ccfbfe06ef3c1d2823d71396
bl2i4n <bl2i4n@yahoo.com>
2017-12-20 17:28:01 -0800

    bug: fix issue with the books thumbnail appearing slightly trimmed

--- src/Book.js
+++ src/Book.js
@@ -29,20 +29,20 @@ class Book extends Component {
           <div className="book-cover" style={ { width: 128, height: 193, backgroundImage: `url("${ thumbnail }")` } }>
           </div>
           <div className="book-shelf-changer">
-            <select
-              value={shelf}
-              onChange={ (event)=> this.changeBookShelf(event.target.value) }
-            >
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
+              <select
+                value={shelf}
+                onChange={ (event)=> this.changeBookShelf(event.target.value) }
+              >
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
           </div>
           <div className="book-title">{title}</div>
           <div className="book-authors">{authors}</div>
-        </div>
       </div>
     );
   }
